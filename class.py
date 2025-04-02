2c38b3f505a6a3cf7412f28476367a933fd7c6c5
cheesemori <2274159818@qq.com>
2025-04-02 16:13:09 +0100

    First ver

---
+++ class.py
@@ -0,0 +1,8 @@
+class Flight:
+    def __init__(self,number,departure,destination,departure_time,arrival_time,seats):
+        self.number = number
+        self.departure = departure
+        self.destination = destination
+        self.departure_time = departure_time
+        self.arrival_time = arrival_time
+
---
+++ main.py
@@ -0,0 +1,3 @@
+def sign_in():
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
