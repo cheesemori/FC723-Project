b7ea9dff2aba951e2e8be38df50c083b3e987278
cheesemori <2274159818@qq.com>
2025-04-03 17:42:37 +0100

    update

---
+++ app_class.py
@@ -0,0 +1,31 @@
+"""class Flight:
+    def __init__(self,number,departure,destination,departure_time,arrival_time,seats):
+        self.number = number
+        self.departure = departure
+        self.destination = destination
+        self.departure_time = departure_time
+        self.arrival_time = arrival_time
+"""
+class Seat:
+    def __init__(self,number):
+        self.number = number
+        self.status = 0
+
+    def __str__(self):
+        if self.status == 0:
+            return str(self.number)
+
+    def turn_available(self):
+        if self.status != 0:
+            self.status = 0
+
+    def turn_occupied(self):
+        if self.status != 1:
+            self.status = 1
+
+    def turn_booked(self):
+        if self.status != 2:
+            self.status = 2
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,43 @@
-def sign_in():
-    username = input("Please enter your username: ")
-    password = input("Please enter your password: ")
+import app_class as c
+
+seat_lst = []
+
+for n in range(0,4):
+    for row in ["A","B","C","X","D","E","F"]:
+        if row == "X":
+            for i in range(20):
+                seat_lst.append("X")
+        else:
+            for col in range(n*20+1,(n+1)*20+1):
+                seat_no = str(col)+row
+                if seat_no in ["77D","77E","77F","78D","78E","78F"]:
+                    seat_lst.append("S")
+                else:
+                    seat_lst.append(seat_no)
+                    seat_lst[-1] = c.Seat(seat_no)
+
+
+
+
+def check_seat():
+    lst_index = 0
+    for i in range(4):
+        for j in range(7):
+            for k in range(20):
+                if seat_lst[lst_index] not in ["X","S"]:
+                    if seat_lst[lst_index].status == 0:
+                        print(f"\033[92m{seat_lst[lst_index]}\033[0m\t", end="   ")
+                    elif seat_lst[lst_index].status == 1:
+                        print(f"\032[92m{seat_lst[lst_index]}\033[0m\t", end="   ")
+                else:
+                    print(f"{seat_lst[lst_index]}\t", end="   ")
+                lst_index += 1
+            print("\n", end="")
+        print("\n")
+
+def find_seat_index(seat_number):
+    lst_index = 0
+
+check_seat()
+
+print(seat_lst)
